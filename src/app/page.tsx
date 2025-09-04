793fff42cc2662244fcf4516b214aa9ad97ffe57
Joodi <winbeta.com@gmail.com>
2025-09-04 17:47:38 +0330

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
+"use client"
+
 import Header from "@/components/Header/header";
 import Image from "next/image";
+import { motion } from "framer-motion"
+import { Badge } from "@/components/ui/badge";
+import { ArrowLeft, Globe, Rocket, Shield, Sparkles, Star, TrendingUp, Zap, PlayCircle, FileX2, LifeBuoy } from "lucide-react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { stats } from "@/lib/data";
+import Typewriter from "@/components/typewriter";
+import HeroSection from "@/components/Home/HeroSection";
+import StatsSection from "@/components/Home/StatsSection";
+import CallToActionSection from "@/components/Home/CallToActionSection";
+
 
 export default function Home() {
+
   return (
     <div className="min-h-screen">
       <Header />
-      میلاد
+      <HeroSection />
+      <StatsSection />
+      <CallToActionSection />
     </div>
   );
 }
